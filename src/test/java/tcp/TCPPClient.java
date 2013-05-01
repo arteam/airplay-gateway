29a193a12e21d1249b07877eab9fadff67e4f218
Artem Prigoda <aprigoda@bcc.ru>
2013-05-01 12:32:56 +0400

    TCPServer and client

---
+++ src/test/java/tcp/TCPPClient.java
@@ -0,0 +1,30 @@
+package tcp;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Date: 01.05.13
+ * Time: 10:33
+ *
+ * @author Artem Prigoda
+ */
+public class TCPPClient {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 9099);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader( socket.getInputStream()));
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            System.out.println("echo: " + in.readLine());
+        }
+
+        out.close();
+        in.close();
+        socket.close();
+        stdIn.close();
+    }
+}
