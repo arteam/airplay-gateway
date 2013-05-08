2464b7756efb6ac8c91117d04751e0eddd0c25b1
Artem Prigoda <arteamon@gmail.com>
2013-05-08 19:02:00 +0400

    HTTP server for content streaming

---
+++ src/test/java/ru/bcc/airstage/airplay/AirPlayGatewayTest.java
@@ -0,0 +1,18 @@
+package ru.bcc.airstage.airplay;
+
+import org.junit.Test;
+
+/**
+ * Date: 08.05.13
+ * Time: 15:45
+ *
+ * @author Artem Prigoda
+ */
+public class AirPlayGatewayTest {
+
+
+    @Test
+    public void playVideoFromITunes() {
+
+    }
+}
