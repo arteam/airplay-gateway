223546a9164a65aac4380b5cdbd446644f9a6696
Artem Prigoda <aprigoda@bcc.ru>
2013-05-08 11:28:41 +0400

    Тест на ConnectionHandler

--- src/main/java/server/command/Response.java
+++ src/main/java/server/command/Response.java
@@ -1,5 +1,7 @@
 package server.command;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Date: 08.05.13
  * Time: 10:31
@@ -9,10 +11,28 @@ package server.command;
 public class Response {
 
     private int code;
+
+    @NotNull
     private Object value;
 
-    public Response(int code, Object value) {
+    public Response() {
+    }
+
+    public Response(int code, @NotNull Object value) {
         this.code = code;
         this.value = value;
+    }
+
+    public static Response ok(@NotNull Object value) {
+        return new Response(0, value);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    @NotNull
+    public Object getValue() {
+        return value;
     }
 }
